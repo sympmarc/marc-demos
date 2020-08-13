b9c690c73d7ae45ee8b509b0eac31fa693d7409e
Marc D Anderson <marc.anderson@sympraxisconsulting.com>
2020-08-12 20:30:44 -0400

    Added Site Design - PnP Provisioning

---
+++ PnPProvisioning/PowerShell/ApplyPnPProvisioningTemplate.ps1
@@ -0,0 +1,38 @@
+#Requires -Module "SharePointPnPPowerShellOnline"
+
+$adminSiteUrl = "https://sympmarc-admin.sharepoint.com"
+
+# Custom functions
+Import-Module "./Powershell/UtilityFunctions.psm1" -Force
+
+$adminConnection = Connect-PnPOnline -Url $adminSiteUrl -Credentials sympmarc -ReturnConnection
+
+# Properties Hub
+$hubSite = "Properties"
+$propertiesHub = Get-PnPHubSite -Identity "https://sympmarc.sharepoint.com/sites/$($hubSite)"
+
+# Set up Project site parameters
+$siteTitle = "foo"
+$propertyCode = "aaa"
+
+# Create the new site
+Write-Host "Creating $($siteTitle)($($propertyCode)) Property Site"
+New-PnPSite -Connection $adminConnection -Type TeamSite -Title $siteTitle -Alias $propertyCode -HubSiteId $propertiesHub.SiteId -IsPublic -Wait
+
+$newSite = Connect-PnPOnline -Url "https://sympmarc.sharepoint.com/sites/$($propertyCode)" -Credentials sympmarc -ReturnConnection
+
+$params = @{
+    "HubSite"      = "$($hubSite)";
+    "SiteTitle"    = "$($siteTitle)";
+    "PropertyCode" = "$($propertyCode)";
+}
+
+# Apply PnP Template
+Apply-PnPProvisioningTemplate `
+    -Connection $newSite `
+    -Path "./PowerShell/IA/PnP XML/PnP-Provisioning-PropertySite.xml" `
+    -Parameters $params
+
+
+
+# Remove-PnPTenantSite -Url $newSite. -Force -SkipRecycleBin
---
+++ SiteDesigns/PowerShell/siteDesign_Demo.ps1
@@ -0,0 +1,25 @@
+$adminSiteUrl = "https://sympmarc-admin.sharepoint.com"
+
+# Custom functions
+Import-Module "./Powershell/UtilityFunctions.psm1" -Force
+
+Connect-PnPOnline -Url $adminSiteUrl -Credential sympmarc
+
+class SiteScript {
+    [string]$SiteScriptName;
+}
+
+$siteScripts = @(
+    [SiteScript]@{SiteScriptName="Property"}
+    [SiteScript]@{SiteScriptName="_DemoBase"}
+)
+
+$siteScriptIds = @()
+
+foreach($ss in $siteScripts)
+{
+    $ssNewId = Add-SiteScript -AdminSiteUrl $adminSiteUrl -SiteScriptFile "./PowerShell/IA/site-scripts/$($ss.SiteScriptName).json" -SiteScriptTitle "$($ss.SiteScriptName)"
+    $siteScriptIds += $ssNewId.Guid
+}
+
+Add-SiteDesign -WebTemplate 64 -SiteDesignTitle "Demo Base" -SiteScriptIds $siteScriptIds
