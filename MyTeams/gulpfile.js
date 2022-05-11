9d8bb5c29b9994a220231f619165f56c4aaf67d2
Marc D Anderson <marc.anderson@sympraxisconsulting.com>
2022-05-11 12:12:15 -0400

    Fixed gulpful / removed test project

--- MyTeams/gulpfile.js
+++ MyTeams/gulpfile.js
@@ -1,26 +1,8 @@
 'use strict';
 
-const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
+
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
-
-
-// added per Waldek's blog post to enable bundled images: https://blog.mastykarz.nl/correctly-reference-images-sharepoint-framework-solutions/
-build.configureWebpack.mergeConfig({
-  additionalConfiguration: (generatedConfiguration) => {
-    if (build.getConfig().production) {
-      var basePath = build.writeManifests.taskConfig.cdnBasePath;
-      if (!basePath.endsWith('/')) {
-        basePath += '/';
-      }
-      generatedConfiguration.output.publicPath = basePath;
-    }
-    else {
-      generatedConfiguration.output.publicPath = "/dist/";
-    }
-    return generatedConfiguration;
-  }
-});
 
 var getTasks = build.rig.getTasks;
 build.rig.getTasks = function () {
@@ -31,4 +13,4 @@ build.rig.getTasks = function () {
   return result;
 };
 
-build.initialize(gulp);
+build.initialize(require('gulp'));
--- test/src/webparts/helloWorld/loc/mystrings.d.ts
+++
@@ -1,7 +0,0 @@
-declare interface IHelloWorldWebPartStrings {
-}
-
-declare module 'HelloWorldWebPartStrings' {
-  const strings: IHelloWorldWebPartStrings;
-  export = strings;
-}
